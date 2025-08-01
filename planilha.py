e1ae11f69b2bd7e458b8e15d7f0d7efddaf19e72
Carlos Notebook <carlosalfredoisensee@gmail.com>
2025-08-01 09:32:33 -0300

    Versão 1.0

--- planilha.py
+++ planilha.py
@@ -1,11 +1,14 @@
+import re
+from xml.sax.handler import property_interning_dict
+
 import requests
 import json
+from openpyxl import load_workbook
 
 url = "https://apiintegracao.milvus.com.br/api/chamado/listagem"
 
 payload = {
     "filtro_body": {
-        # "tipo_ticket": "Novo Colaborador"
         "tipo_ticket": "Novo Colaborador"
     }
 }
@@ -20,8 +23,38 @@ response.raise_for_status()
 
 dados = response.json()
 
+wb = load_workbook("Criação de Acessos.xlsx")
+ws = wb["Planilha1"]
+
+contador = 0
+
+for row in ws.iter_rows(min_row=2, min_col=1, max_col=1, values_only=True):
+    valor = row[0]
+
+    if isinstance(valor, (int, float)) and 1000 <= valor <= 9999:
+        contador += 1
+
+linha_atual = contador + 2
+
 for chamado in dados.get("lista", []):
-    # Seperar os dados em colunas para enviar para o excel
-    print(f'{chamado["codigo"]}')
-    print(f'{chamado.get("assunto")}')
-    print(f'{chamado.get("descricao")}')
+    ws.cell(row=linha_atual, column=1).value = chamado["codigo"]
+
+    padrao = re.compile(
+        r'^(?P<setor_lider>[\w\s]+\/[\w\s]+)\s+[–-]\s+(?P<nome>.+?)\s+[–-]\s+(?P<data>\d{2}/\d{2})\s+[–-]\s+(?P<local>.+?)\s*(?:-[–-]\s*Vaga\s+\d+)?$',
+        re.UNICODE
+    )
+
+    print()
+    match = padrao.match(chamado["assunto"])
+    if match:
+        ws.cell(row=linha_atual, column=2).value = match.group("setor_lider")
+        ws.cell(row=linha_atual, column=3).value = match.group("nome")
+        ws.cell(row=linha_atual, column=4).value = match.group("data")
+        ws.cell(row=linha_atual, column=5).value = match.group("local")
+
+    linha_atual += 1
+
+
+
+wb.save("Criação de Acessos.xlsx")
+print("Chamado foi")
